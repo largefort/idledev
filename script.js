b83b9574d39d45946b9b57bff793089784a2750b
largefort <106451813+largefort@users.noreply.github.com>
2023-06-13 19:55:40 +0000

    Update script.js

--- script.js
+++ script.js
@@ -28,21 +28,21 @@ function hireDeveloper() {
 
 function createGame() {
   const gameName = document.getElementById("gameName").value;
-
+  
   if (gameName.trim() === "") {
     alert("Please enter a game name!");
     return;
   }
-
+  
   const game = {
     name: gameName,
     pointsPerDeveloper: 2,
     pointsGenerated: 0
   };
-
+  
   games.push(game);
   document.getElementById("gameName").value = "";
-
+  
   // Display the created game
   const gameContainer = document.createElement("div");
   gameContainer.className = "game-container";
@@ -50,14 +50,65 @@ function createGame() {
     <h3>${game.name}</h3>
     <p>Points Generated: <span id="pointsGenerated_${games.length - 1}">0</span></p>
   `;
-
+  
   document.body.appendChild(gameContainer);
 }
 
-// Initialize the game
-function initGame() {
-  updatePoints();
-  updateDevelopers();
+function updateGamePoints() {
+  games.forEach((game, index) => {
+    const pointsGeneratedElement = document.getElementById(`pointsGenerated_${index}`);
+    pointsGeneratedElement.textContent = game.pointsGenerated;
+  });
 }
 
-initGame();
+function exportSave() {
+  const saveData = {
+    points: points,
+    developers: developers,
+    games: games
+  };
+
+  const saveString = JSON.stringify(saveData);
+  const encodedSave = btoa(saveString);
+  const exportData = `IdleGameSaveData:${encodedSave}`;
+
+  const saveElement = document.createElement("a");
+  saveElement.href = "data:text/plain;charset=utf-8," + encodeURIComponent(exportData);
+  saveElement.download = "idle_game_save.txt";
+  saveElement.click();
+}
+
+function importSave() {
+  const importData = document.getElementById("importSaveInput").value;
+  
+  if (!importData.startsWith("IdleGameSaveData:")) {
+    alert("Invalid save data!");
+    return;
+  }
+
+  const encodedSave = importData.replace("IdleGameSaveData:", "");
+  const saveString = atob(encodedSave);
+  const saveData = JSON.parse(saveString);
+
+  points = saveData.points;
+  developers = saveData.developers;
+  games = saveData.games;
+
+  updatePoints();
+  updateDevelopers();
+  updateGamePoints();
+
+  document.getElementById("importSaveInput").value = "";
+}
+
+// Automatic points generation by developers every second
+setInterval(function() {
+  points += developers;
+  
+  games.forEach((game) => {
+    game.pointsGenerated += game.pointsPerDeveloper * developers;
+  });
+  
+  updatePoints();
+  updateGamePoints();
+}, 1000);
