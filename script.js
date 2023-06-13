66f2eae3e78d2e7cef8804fd06bb772d7c5c2d07
largefort <106451813+largefort@users.noreply.github.com>
2023-06-13 19:33:55 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let points = 0;
 let developers = 0;
 let games = [];
+let totalPlayers = 0;
+let totalRevenue = 0;
 
 function updatePoints() {
   document.getElementById("points").textContent = points;
@@ -37,7 +39,9 @@ function createGame() {
   const game = {
     name: gameName,
     pointsPerDeveloper: 2,
-    pointsGenerated: 0
+    pointsGenerated: 0,
+    players: 0,
+    revenue: 0
   };
 
   games.push(game);
@@ -49,16 +53,38 @@ function createGame() {
   gameContainer.innerHTML = `
     <h3>${game.name}</h3>
     <p>Points Generated: <span id="pointsGenerated_${games.length - 1}">0</span></p>
+    <p>Players: <span id="players_${games.length - 1}">0</span></p>
+    <p>Revenue: $<span id="revenue_${games.length - 1}">0</span></p>
   `;
 
   document.body.appendChild(gameContainer);
+
+  updateGameStats();
+}
+
+function updateGameStats() {
+  const totalGames = games.length;
+  let players = 0;
+  let revenue = 0;
+
+  for (let i = 0; i < games.length; i++) {
+    players += games[i].players;
+    revenue += games[i].revenue;
+  }
+
+  totalPlayers = players;
+  totalRevenue = revenue;
+
+  document.getElementById("totalGames").textContent = totalGames;
+  document.getElementById("totalPlayers").textContent = totalPlayers;
+  document.getElementById("totalRevenue").textContent = totalRevenue;
 }
 
 // Initialize the game
 function initGame() {
   updatePoints();
   updateDevelopers();
+  updateGameStats();
 }
 
 initGame();
-
