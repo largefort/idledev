096819b9e79b1a238979c326c630b899894e650d
largefort <106451813+largefort@users.noreply.github.com>
2023-06-13 19:53:16 +0000

    Update script.js

--- script.js
+++ script.js
@@ -28,21 +28,21 @@ function hireDeveloper() {
 
 function createGame() {
   const gameName = document.getElementById("gameName").value;
-  
+
   if (gameName.trim() === "") {
     alert("Please enter a game name!");
     return;
   }
-  
+
   const game = {
     name: gameName,
     pointsPerDeveloper: 2,
     pointsGenerated: 0
   };
-  
+
   games.push(game);
   document.getElementById("gameName").value = "";
-  
+
   // Display the created game
   const gameContainer = document.createElement("div");
   gameContainer.className = "game-container";
@@ -50,25 +50,14 @@ function createGame() {
     <h3>${game.name}</h3>
     <p>Points Generated: <span id="pointsGenerated_${games.length - 1}">0</span></p>
   `;
-  
+
   document.body.appendChild(gameContainer);
 }
 
-function updateGamePoints() {
-  games.forEach((game, index) => {
-    const pointsGeneratedElement = document.getElementById(`pointsGenerated_${index}`);
-    pointsGeneratedElement.textContent = game.pointsGenerated;
-  });
+// Initialize the game
+function initGame() {
+  updatePoints();
+  updateDevelopers();
 }
 
-// Automatic points generation by developers every second
-setInterval(function() {
-  points += developers;
-  
-  games.forEach((game) => {
-    game.pointsGenerated += game.pointsPerDeveloper * developers;
-  });
-  
-  updatePoints();
-  updateGamePoints();
-}, 1000);
+initGame();
