49449a0255b1b7828b6da781fea78097ced1aaa0
largefort <106451813+largefort@users.noreply.github.com>
2023-06-13 19:06:02 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let points = 0;
 let developers = 0;
+let games = [];
 
 function updatePoints() {
   document.getElementById("points").textContent = points;
@@ -25,8 +26,49 @@ function hireDeveloper() {
   }
 }
 
+function createGame() {
+  const gameName = document.getElementById("gameName").value;
+  
+  if (gameName.trim() === "") {
+    alert("Please enter a game name!");
+    return;
+  }
+  
+  const game = {
+    name: gameName,
+    pointsPerDeveloper: 2,
+    pointsGenerated: 0
+  };
+  
+  games.push(game);
+  document.getElementById("gameName").value = "";
+  
+  // Display the created game
+  const gameContainer = document.createElement("div");
+  gameContainer.className = "game-container";
+  gameContainer.innerHTML = `
+    <h3>${game.name}</h3>
+    <p>Points Generated: <span id="pointsGenerated_${games.length - 1}">0</span></p>
+  `;
+  
+  document.body.appendChild(gameContainer);
+}
+
+function updateGamePoints() {
+  games.forEach((game, index) => {
+    const pointsGeneratedElement = document.getElementById(`pointsGenerated_${index}`);
+    pointsGeneratedElement.textContent = game.pointsGenerated;
+  });
+}
+
 // Automatic points generation by developers every second
 setInterval(function() {
   points += developers;
+  
+  games.forEach((game) => {
+    game.pointsGenerated += game.pointsPerDeveloper * developers;
+  });
+  
   updatePoints();
+  updateGamePoints();
 }, 1000);
