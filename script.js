5cf4a99f6053fcd338cabf8e8395e25ca8731501
largefort <106451813+largefort@users.noreply.github.com>
2023-06-13 19:04:00 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+let points = 0;
+let developers = 0;
+
+function updatePoints() {
+  document.getElementById("points").textContent = points;
+}
+
+function incrementPoints() {
+  points++;
+  updatePoints();
+}
+
+function updateDevelopers() {
+  document.getElementById("developers").textContent = developers;
+}
+
+function hireDeveloper() {
+  if (points >= 10) {
+    points -= 10;
+    developers++;
+    updatePoints();
+    updateDevelopers();
+  } else {
+    alert("Insufficient points to hire a developer!");
+  }
+}
+
+// Automatic points generation by developers every second
+setInterval(function() {
+  points += developers;
+  updatePoints();
+}, 1000);
