1b6ae0a8f70bdb14d30a6d0b20ea4a3ea30e7b54
largefort <106451813+largefort@users.noreply.github.com>
2023-06-14 17:21:03 +0000

    Update script.js

--- script.js
+++ script.js
@@ -194,15 +194,3 @@ for (let i = 0; i < 3; i++) {
 window.addEventListener("load", function() {
   loadGame();
 });
-
-function requestFullscreen(element) {
-  if (element.requestFullscreen) {
-    element.requestFullscreen();
-  } else if (element.mozRequestFullScreen) {
-    element.mozRequestFullScreen();
-  } else if (element.webkitRequestFullscreen) {
-    element.webkitRequestFullscreen();
-  } else if (element.msRequestFullscreen) {
-    element.msRequestFullscreen();
-  }
-}
