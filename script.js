e3e4c2cea078c898d5b2c9952668fecb16b29923
largefort <106451813+largefort@users.noreply.github.com>
2023-07-16 19:37:46 +0000

    Update script.js

--- script.js
+++ script.js
@@ -28,21 +28,21 @@ function hireDeveloper() {
 
 function createGame() {
   const gameName = document.getElementById("gameName").value;
-  
+
   if (gameName.trim() === "") {
     alert("Please enter a game name!");
     return;
   }
-  
+
   const game = {
     name: gameName,
     pointsPerDeveloper: 2,
     pointsGenerated: 0
   };
-  
+
   games.push(game);
   document.getElementById("gameName").value = "";
-  
+
   // Display the created game
   const gameContainer = document.createElement("div");
   gameContainer.className = "game-container";
@@ -50,9 +50,9 @@ function createGame() {
     <h3>${game.name}</h3>
     <p>Points Generated: <span id="pointsGenerated_${games.length - 1}">0</span></p>
   `;
-  
+
   document.body.appendChild(gameContainer);
-  
+
   autosave();
 }
 
@@ -69,20 +69,20 @@ function autosave() {
     developers: developers,
     games: games
   };
-  
+
   localStorage.setItem("idleGameSave", JSON.stringify(saveData));
 }
 
 function loadSave() {
   const saveData = localStorage.getItem("idleGameSave");
-  
+
   if (saveData) {
     const save = JSON.parse(saveData);
-    
+
     points = save.points;
     developers = save.developers;
     games = save.games;
-    
+
     updatePoints();
     updateDevelopers();
     updateGamePoints();
@@ -92,14 +92,14 @@ function loadSave() {
 // Automatic points generation by developers every second
 setInterval(function() {
   points += developers;
-  
+
   games.forEach((game) => {
     game.pointsGenerated += game.pointsPerDeveloper * developers;
   });
-  
+
   updatePoints();
   updateGamePoints();
-  
+
   autosave();
 }, 1000);
 
