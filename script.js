cb0ef61e5984592dda5f967432f1477bd8808cff
largefort <106451813+largefort@users.noreply.github.com>
2023-07-25 13:50:17 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,150 +1,74 @@
-// Game variables
-let money = 0;
-let gameIdeas = 0;
-let isCreatingGame = false;
-let gameIncome = 0;
+document.addEventListener("DOMContentLoaded", () => {
+  let money = 0;
+  let gamesDeveloped = 0;
+  let linesOfCode = 0;
+  let employees = 0;
 
-// Building data
-const buildings = [
-  { name: "Small Office", cost: 100, income: 10 },
-  { name: "Game Studio", cost: 500, income: 50 },
-  { name: "Indie Studio", cost: 1000, income: 100 },
-  { name: "AAA Studio", cost: 5000, income: 500 },
-  { name: "Game Publisher", cost: 10000, income: 1000 },
-  { name: "Gaming Convention", cost: 50000, income: 5000 },
-  { name: "Esports Arena", cost: 100000, income: 10000 },
-  // Add more buildings here
-];
+  const moneyElement = document.getElementById("money");
+  const gamesElement = document.getElementById("games");
+  const linesOfCodeElement = document.getElementById("linesOfCode");
+  const employeesElement = document.getElementById("employees");
+  const developBtn = document.getElementById("developBtn");
+  const hireBtn = document.getElementById("hireBtn");
 
-// Create building elements
-function createBuildings() {
-  const buildingContainer = document.getElementById("building-container");
-  buildingContainer.innerHTML = "";
-
-  buildings.forEach((building, index) => {
-    const buildingElem = document.createElement("div");
-    buildingElem.classList.add("building");
-    buildingElem.innerHTML = `
-      <p>${building.name}</p>
-      <p>Cost: $${building.cost}</p>
-      <p>Income: $${building.income}/s</p>
-      <button onclick="buyBuilding(${index})">Buy</button>
-    `;
-    buildingContainer.appendChild(buildingElem);
-  });
-}
-
-// Buy a building
-function buyBuilding(index) {
-  const building = buildings[index];
-  if (money >= building.cost) {
-    money -= building.cost;
-    gameIncome += building.income;
-    updateMoney();
-  }
-}
-
-// Create a new game
-function createGame() {
-  if (gameIdeas >= 10) {
-    gameIdeas -= 10;
-    isCreatingGame = true;
-    document.getElementById("game-creation-container").style.display = "block";
-    document.getElementById("create-game-btn").disabled = true;
-    updateGameIdeas();
-  }
-}
-
-// Confirm game creation
-function confirmGameCreation() {
-  const gameNameInput = document.getElementById("game-name-input");
-  const gameSuccessInput = document.getElementById("game-success-input");
-  const gameName = gameNameInput.value;
-  const gameSuccess = parseInt(gameSuccessInput.value);
-
-  if (gameName.trim() !== "" && !isNaN(gameSuccess)) {
-    // Create a new game with the given name and success level
-    // You can implement this part on your own.
-    // ...
-
-    // Hide game creation UI
-    isCreatingGame = false;
-    document.getElementById("game-creation-container").style.display = "none";
-    document.getElementById("create-game-btn").disabled = false;
-
-    gameNameInput.value = "";
-    gameSuccessInput.value = "";
-  }
-}
-
-// Cancel game creation
-function cancelGameCreation() {
-  isCreatingGame = false;
-  document.getElementById("game-creation-container").style.display = "none";
-  document.getElementById("create-game-btn").disabled = false;
-}
-
-// Generate money based on game success
-function generateMoney() {
-  money += gameIncome;
-  updateMoney();
-}
-
-// Update money display
-function updateMoney() {
-  document.getElementById("money").textContent = money;
-}
-
-// Update game ideas display
-function updateGameIdeas() {
-  document.getElementById("game-ideas").textContent = gameIdeas;
-}
-
-// Save game data to Local Storage
-function saveGame() {
-  const gameData = {
-    money,
-    gameIdeas,
-    gameIncome,
-    buildings
-    // Add other data that you want to save
-  };
-  localStorage.setItem("gameData", JSON.stringify(gameData));
-  alert("Game saved!");
-}
-
-// Load game data from Local Storage
-function loadGame() {
-  const savedData = localStorage.getItem("gameData");
+  // Load player progress from localStorage (if available)
+  const savedData = JSON.parse(localStorage.getItem("gameData"));
   if (savedData) {
-    const gameData = JSON.parse(savedData);
-    money = gameData.money;
-    gameIdeas = gameData.gameIdeas;
-    gameIncome = gameData.gameIncome;
-    // Restore purchased buildings
-    buildings.forEach((building, index) => {
-      building.cost = gameData.buildings[index].cost;
-      building.income = gameData.buildings[index].income;
-    });
-    // Update other variables and UI as per your game's requirements
-    updateMoney();
-    updateGameIdeas();
-    createBuildings();
-    alert("Game loaded!");
-  } else {
-    alert("No saved game found!");
+    money = savedData.money;
+    gamesDeveloped = savedData.gamesDeveloped;
+    linesOfCode = savedData.linesOfCode;
+    employees = savedData.employees;
   }
-}
 
-// Initialization
-document.getElementById("create-game-btn").addEventListener("click", createGame);
-document.getElementById("confirm-game-btn").addEventListener("click", confirmGameCreation);
-document.getElementById("cancel-game-btn").addEventListener("click", cancelGameCreation);
-document.getElementById("save-btn").addEventListener("click", saveGame);
-document.getElementById("load-btn").addEventListener("click", loadGame);
-createBuildings();
-updateMoney();
-updateGameIdeas();
+  // Update UI with the initial values
+  moneyElement.textContent = money;
+  gamesElement.textContent = gamesDeveloped;
+  linesOfCodeElement.textContent = linesOfCode;
+  employeesElement.textContent = employees;
+
+  // Handle the click event on the Develop Game button
+  developBtn.addEventListener("click", () => {
+    const linesDeveloped = employees * 10; // Each employee develops 10 lines of code
+    linesOfCode += linesDeveloped;
+    money += linesDeveloped * 0.1; // Earn $0.1 for each line of code developed
+    gamesDeveloped++;
+    updateUI();
+
+    // Save the player's progress in localStorage
+    saveGame();
+  });
+
+  // Handle the click event on the Hire Employee button
+  hireBtn.addEventListener("click", () => {
+    if (money >= 50) {
+      money -= 50;
+      employees++;
+      updateUI();
+      saveGame();
+    } else {
+      alert("Not enough money to hire an employee!");
+    }
+  });
+
+  // Function to update the UI
+  function updateUI() {
+    moneyElement.textContent = money.toFixed(2); // Display money with two decimal places
+    gamesElement.textContent = gamesDeveloped;
+    linesOfCodeElement.textContent = linesOfCode;
+    employeesElement.textContent = employees;
+  }
+
+  // Function to save the game progress in localStorage
+  function saveGame() {
+    const gameData = {
+      money: money,
+      gamesDeveloped: gamesDeveloped,
+      linesOfCode: linesOfCode,
+      employees: employees,
+    };
+    localStorage.setItem("gameData", JSON.stringify(gameData));
+  }
+});
 
 // Automatic money generation
 setInterval(generateMoney, 1000); // Generate money every second
