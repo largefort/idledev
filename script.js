9fd4b651be12bb05c6d1d04aaf3a4440e9a9483a
largefort <106451813+largefort@users.noreply.github.com>
2023-07-25 14:15:28 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let locPerClick = 1;
 let upgradeCost = 10;
 let upgradeLevel = 1;
 let locPerCode = 5;
+let autoCodeWritingUpgradeCost = 100;
+let autoCodeWritingInterval = null;
+let achievements = [];
 
 const currencyElement = document.getElementById("currency");
 const workButton = document.getElementById("workButton");
@@ -10,6 +13,8 @@ const upgradeButton = document.getElementById("upgradeButton");
 const upgradeLevelElement = document.getElementById("upgradeLevel");
 const codeInput = document.getElementById("codeInput");
 const runButton = document.getElementById("runButton");
+const firstAchievement = document.getElementById("firstAchievement");
+const achievementsList = document.getElementById("achievements");
 
 function updateCurrency() {
   currencyElement.textContent = currency;
@@ -19,52 +24,40 @@ function updateUpgradeLevel() {
   upgradeLevelElement.textContent = upgradeLevel;
 }
 
-function generateRandomCodeLines(language) {
-  const languages = {
-    javascript: () => {
-      const functionName = "exampleFunction";
-      return `
-function ${functionName}() {
-  // Your JavaScript code here...
-}
-      `;
-    },
-    python: () => {
-      const functionName = "example_function";
-      return `
-def ${functionName}():
-    # Your Python code here...
-      `;
-    },
-    java: () => {
-      const functionName = "exampleFunction";
-      return `
-public static void ${functionName}() {
-    // Your Java code here...
-}
-      `;
-    }
-    // Add more languages as needed
-  };
-
-  if (languages[language.toLowerCase()]) {
-    return languages[language.toLowerCase()]();
-  } else {
-    throw new Error(`Language '${language}' not supported.`);
-  }
+function generateRandomCodeLines() {
+  const linesOfCode = Math.floor(Math.random() * 5) + 1; // Generate between 1 to 5 lines of code
+  return "function exampleFunction() {\n" + "  // Your code here...\n".repeat(linesOfCode) + "}";
 }
 
 function work() {
   currency += locPerClick;
   updateCurrency();
 
-  const language = "javascript"; // Change this to the desired language
-  const generatedCode = generateRandomCodeLines(language);
-  codeInput.value = generatedCode;
+  if (currency >= autoCodeWritingUpgradeCost && autoCodeWritingInterval === null) {
+    autoCodeWritingInterval = setInterval(() => {
+      const generatedCode = generateRandomCodeLines();
+      codeInput.value = generatedCode;
+      runCode(); // Automatically run the generated code
+    }, 5000); // Auto code writing interval in milliseconds (5 seconds)
+  }
 }
 
 function upgrade() {
-  // ... (same as before) ...
+  if (currency >= upgradeCost) {
+    currency -= upgradeCost;
+    locPerClick += upgradeLevel;
+    upgradeLevel++;
+    upgradeCost *= 2;
+    updateCurrency();
+    updateUpgradeLevel();
+
+    if (achievements.indexOf("firstAchievement") === -1 && upgradeLevel >= 5) {
+      achievements.push("firstAchievement");
+      achievementsList.innerHTML += "<li>First Achievement Unlocked: Reached Upgrade Level 5!</li>";
+    }
+  } else {
+    alert("Not enough LOC to purchase the upgrade!");
+  }
 }
 
 function runCode() {
